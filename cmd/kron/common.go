e1c567a1854db337367237b5c1307823d82059c9
Justin Pu <12262539+aurpine@users.noreply.github.com>
2019-06-04 13:58:02 -0700

    Add favorite and unfavorite commands

---
+++ cmd/kron/common.go
@@ -0,0 +1,32 @@
+package kron
+
+import (
+  "os"
+  "fmt"
+  "github.com/spf13/viper"
+)
+
+// For storing the location of a job for select and favorite.
+type location struct {
+	Contexts []string
+	Namespaces []string
+}
+
+func toLocation(obj interface{}) location {
+  m, ok := obj.(map[string]interface{})
+  if !ok {
+    fmt.Println("Failed")
+    return location{} // maybe panic??
+  }
+  c := m["contexts"].([]string)
+  n := m["namespaces"].([]string)
+  return location{c, n}
+}
+
+func createConfig() {
+  os.Mkdir(os.Getenv("HOME") + "/.kron/", 0777)
+  err := viper.WriteConfigAs(os.Getenv("HOME") + "/.kron/config.yaml")
+  if err != nil {
+    panic(err.Error())
+  }
+}
