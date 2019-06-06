45af63ce3c8c452d089544f1f0deff149db56fcc
Justin Pu <12262539+aurpine@users.noreply.github.com>
2019-06-05 18:02:39 -0700

    Refactor (redo) get command to use client-go

--- cmd/get.go
+++ cmd/get.go
@@ -1,112 +1,49 @@
 package cmd
 
 import (
-	"fmt"
-	"os"
-	"os/exec"
-	"strings"
-
-	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
+  "fmt"
+  "github.com/ContextLogic/ctl/pkg/client"
+  "text/tabwriter"
+  "github.com/spf13/cobra"
+  "os"
+  "time"
 )
 
 func init() {
-	rootCmd.AddCommand(getCmd)
-	getCmd.Flags().StringP("namespace", "n", "", "Specify the namespace")
-	getCmd.Flags().StringP("region", "r", "", "Specify the region")
-	getCmd.Flags().StringP("env", "e", "", "Specify the enviroment")
-	getCmd.Flags().StringP("az", "a", "", "Specify the alvalibility zone")
-	getCmd.Flags().StringP("config", "", "", "Specify the config file")
-
+  rootCmd.AddCommand(getCmd)
+  getCmd.Flags().StringP("namespace", "n", "", "Specify the namespace")
+  getCmd.Flags().StringSliceP("context", "c", []string{}, "Specify the context")
 }
 
 var getCmd = &cobra.Command{
-	Use:   "get [flags]",
-	Short: "Get a list of pods",
-	Long:  "Get a list of pods in namespace. If namespace not specified, it will get all the pods across all the namespaces",
-	Run: func(cmd *cobra.Command, args []string) {
-		namespace, _ := cmd.Flags().GetString("namespace")
-		region, _ := cmd.Flags().GetString("region")
-		env, _ := cmd.Flags().GetString("env")
-		az, _ := cmd.Flags().GetString("az")
-		config, _ := cmd.Flags().GetString("config")
-		getPodsFromClustersByNamespace(namespace, config, region, env, az)
-	},
-	Args:    cobra.MaximumNArgs(0),
-	Aliases: []string{"gets"},
-}
+  Use: "get [flags]",
+  Short: "Get a list of pods",
+  Long: `Get a list of pods in specified namespace and context(s).
+    If namespace not specified, it will get all the pods across all the namespaces.
+    If context(s) not specified, it will go through all contexts.`,
+  Run: func(cmd *cobra.Command, args []string) {
+    ctxs, _ := cmd.Flags().GetStringSlice("contexts")
+    namespace, _ := cmd.Flags().GetString("namespace")
 
-func getPodsInNamespaceInCluster(cluster, namespace string, resultChannel chan PodList) {
+    list, err := client.GetDefaultConfigClient().
+      ListPodsOverContexts(ctxs, namespace, client.ListOptions{0})
 
-	var tempPodList PodList
-	var command *exec.Cmd
+    if err != nil {
+      panic(err.Error())
+    }
 
-	if namespace == "" {
-		command = exec.Command(
-			"kubectl",
-			"get", "pods",
-			"--all-namespaces",
-			//"-o=custom-columns=NAME:.metadata.name",
-			fmt.Sprintf("--context=%s", cluster))
-	} else {
-		command = exec.Command(
-			"kubectl",
-			"get", "pods",
-			"-n", namespace,
-			//"-o=custom-columns=NAME:.metadata.name",
-			fmt.Sprintf("--context=%s", cluster))
-	}
+    w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', tabwriter.TabIndent)
+    fmt.Fprintln(w, "CONTEXT\tNAMESPACE\tNAME\tREADY\tSTATUS\tRESTARTS\tAGE")
 
-	if viper.GetBool("verbose") {
-		prettyPrintCmd(command)
-	}
-
-	result, err := command.Output()
-
-	if err != nil || string(result) == "" {
-		resultChannel <- tempPodList
-		return
-	}
-
-	podList := strings.Split(strings.TrimSpace(string(result)), "\n")
-	if len(podList) < 2 {
-		resultChannel <- tempPodList
-		return
-	}
-
-	resultChannel <- PodList{cluster, podList}
-}
-
-func getPodsFromClustersByNamespace(namespace, configpath, region, enviroment, az string) {
-	clusters, err := getFilteredClusters(configpath, region, enviroment, az)
-	if err != nil {
-		fmt.Printf("failed to get clusters: %v\n", err)
-		os.Exit(1)
-	}
-	if len(clusters) == 0 {
-		fmt.Print("failed to get clusters: no cluster found\n")
-		os.Exit(1)
-	}
-
-	resultChan := make(chan PodList)
-
-	for _, c := range clusters {
-		go getPodsInNamespaceInCluster(c, namespace, resultChan)
-	}
-
-	var result []PodList
-
-	for range clusters {
-		response := <-resultChan
-		if len(response.Pods) == 0 {
-			continue
-		}
-
-		fmt.Print(response)
-		result = append(result, response)
-	}
-
-	if len(result) == 0 {
-		fmt.Println("No pod found.")
-	}
+    for _, v := range list {
+      fmt.Fprintf(w, "%s\t", v.Context)
+      fmt.Fprintf(w, "%s\t", v.Namespace)
+      fmt.Fprintf(w, "%s\t", v.Name)
+      fmt.Fprintf(w, "TODO\t")
+      fmt.Fprintf(w, "%v\t", v.Status.Phase)
+      fmt.Fprintf(w, "TODO\t")
+      fmt.Fprintf(w, "%v\n", time.Since(v.CreationTimestamp.Time).Round(time.Second))
+    }
+    w.Flush()
+  },
 }
---
+++ pkg/client/listpods.go
@@ -0,0 +1,56 @@
+package client
+
+import (
+	"sync"
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"github.com/ContextLogic/ctl/pkg/client/helper"
+)
+
+type PodDiscovery struct {
+	Context string
+	Namespace string
+	v1.Pod
+}
+
+func (c *Client) ListPods(context string, namespace string, options ListOptions) ([]v1.Pod, error) {
+	cs, err := c.getContextClientset(context)
+	if err != nil {
+		return nil, err
+	}
+	pods, err := cs.CoreV1().Pods(namespace).List(metav1.ListOptions{Limit: options.Limit})
+	if err != nil {
+		return nil, err
+	}
+	return pods.Items, nil
+}
+
+func (c *Client) ListPodsOverContexts(contexts []string, namespace string, options ListOptions) ([]PodDiscovery, error) {
+	if len(contexts) == 0 {
+		contexts = helper.GetContexts()
+	}
+
+	var wait sync.WaitGroup
+	wait.Add(len(contexts))
+
+	var mutex sync.Mutex
+	var ret []PodDiscovery
+
+	for _, ctx := range contexts {
+		go func(ctx string) {
+			defer wait.Done()
+
+			list, err := c.ListPods(ctx, namespace, options)
+			if err != nil { return }
+
+			mutex.Lock()
+			for _, x := range list {
+				ret = append(ret, PodDiscovery{ctx, x.Namespace, x})
+			}
+			mutex.Unlock()
+		}(ctx)
+	}
+
+	wait.Wait()
+	return ret, nil
+}
