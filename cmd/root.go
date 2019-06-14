87cc0eaf203eb23851cbfbfa61f789d804238c3d
Justin Pu <12262539+aurpine@users.noreply.github.com>
2019-06-13 17:30:05 -0700

    Remove verbose flag

--- cmd/root.go
+++ cmd/root.go
@@ -3,18 +3,13 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"os/exec"
-	"os/user"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 func init() {
-	rootCmd.PersistentFlags().BoolP("verbose", "v", false, "print out verbose msg")
 	rootCmd.PersistentFlags().StringSliceP("context", "c", nil, "Specify the context(s) to operate in")
 	rootCmd.PersistentFlags().StringP("namespace", "n", "", "Specify the namespace within all the contexts specified")
-	viper.BindPFlag("verbose", rootCmd.PersistentFlags().Lookup("verbose"))
 }
 
 // rootCmd represents the base command when called without any subcommands
@@ -22,25 +17,13 @@ var rootCmd = &cobra.Command{
 	Use:   "ctl",
 	Short: "A CLI tool for discovering k8s pods/logs across multiple clusters",
 	Long:  `ctl is a CLI tool for easily getting/exec pods/logs across multiple clusters/namespaces. If you have any questions, problems, or requests please ask #automation.`,
-	PersistentPreRun: func(cmd *cobra.Command, args []string) {
-		_, err := exec.LookPath("kubectl")
-		if err != nil {
-			fmt.Println("kubectl not installed. k8s commands are unavaible.")
-			os.Exit(1)
-		}
-		currentUser, _ := user.Current()
-		_, err = os.Stat(currentUser.HomeDir + "/.kube/config")
-		if os.IsNotExist(err) {
-			fmt.Println("missing kubeconfig. k8s commands are unavaible.")
-			os.Exit(1)
-		}
-	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
 }
