ac62ca8cd4ee2516580276cc492f669177676870
Justin Pu <12262539+aurpine@users.noreply.github.com>
2019-07-16 16:03:25 -0700

    Remove init, improve cmd consistency, fix ci

--- cmd/root.go
+++ cmd/root.go
@@ -7,32 +7,34 @@ import (
 	"os"
 )
 
-func init() {
-	rootCmd.PersistentFlags().StringSliceP("context", "x", nil, "Specify the context(s) to operate in")
-	rootCmd.PersistentFlags().StringP("namespace", "n", "", "Specify the namespace within all the contexts specified")
-	rootCmd.PersistentFlags().StringArrayP("label", "l", nil, "Filter objects by label")
+func cmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:          "ctl",
+		Short:        "A CLI tool for discovering k8s pods/logs across multiple clusters",
+		Long:         `ctl is a CLI tool for easily getting/exec pods/logs across multiple clusters/namespaces. If you have any questions, problems, or requests please ask #automation.`,
+		SilenceUsage: true,
+	}
 
-	// Commands'
+	// If config flag is set here...
 	c := client.GetDefaultConfigClient()
-	rootCmd.AddCommand(describeCmd(c))
-	rootCmd.AddCommand(getCmd(c))
-	rootCmd.AddCommand(logsCmd(c))
-	rootCmd.AddCommand(shCmd(c))
-	rootCmd.AddCommand(kron.Cmd(c))
-}
+	cmd.AddCommand(describeCmd(c))
+	cmd.AddCommand(getCmd(c))
+	cmd.AddCommand(logsCmd(c))
+	cmd.AddCommand(shCmd(c))
+	cmd.AddCommand(versionCmd(c))
+	cmd.AddCommand(kron.Cmd(c))
 
-// rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:          "ctl",
-	Short:        "A CLI tool for discovering k8s pods/logs across multiple clusters",
-	Long:         `ctl is a CLI tool for easily getting/exec pods/logs across multiple clusters/namespaces. If you have any questions, problems, or requests please ask #automation.`,
-	SilenceUsage: true,
+	cmd.PersistentFlags().StringSliceP("context", "x", nil, "Specify the context(s) to operate in")
+	cmd.PersistentFlags().StringP("namespace", "n", "", "Specify the namespace within all the contexts specified")
+	cmd.PersistentFlags().StringArrayP("label", "l", nil, "Filter objects by label")
+
+	return cmd
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	if err := rootCmd.Execute(); err != nil {
+	if err := cmd().Execute(); err != nil {
 		// No printing of err needed because it already errors??
 		os.Exit(1)
 	}
--- cmd/version.go
+++ cmd/version.go
@@ -2,19 +2,18 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
+	"github.com/wish/ctl/pkg/client"
 )
 
 // Version set default value
 var Version = "unset"
 
-func init() {
-	rootCmd.AddCommand(versionCmd)
-}
-
-var versionCmd = &cobra.Command{
-	Use:   "version",
-	Short: "Show ctl version",
-	Run: func(cmd *cobra.Command, args []string) {
-		cmd.Println(rootCmd.Use + " version: " + Version)
-	},
+func versionCmd(*client.Client) *cobra.Command {
+	return &cobra.Command{
+		Use:   "version",
+		Short: "Show ctl version",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Println(cmd.Use + " version: " + Version)
+		},
+	}
 }
