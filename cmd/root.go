25e204a33440e298d2e3055327d5cb8033a9f3be
Justin Pu <12262539+aurpine@users.noreply.github.com>
2019-07-17 15:40:04 -0700

    Add kubeconfig flag

--- cmd/root.go
+++ cmd/root.go
@@ -6,18 +6,29 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/wish/ctl/cmd/kron"
 	"github.com/wish/ctl/pkg/client"
+	"os"
+	// "path/filepath"
 )
 
 func cmd() *cobra.Command {
+	// Placeholder client
+	c := client.GetPlaceholderClient()
+
 	cmd := &cobra.Command{
 		Use:          "ctl",
 		Short:        "A CLI tool for discovering k8s pods/logs across multiple clusters",
 		Long:         "ctl is a CLI tool for easily getting/exec pods/logs across multiple clusters/namespaces.",
 		SilenceUsage: true,
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			// REVIEW: this is quite sketchy. Should use another method
+			if k, _ := cmd.Flags().GetString("kubeconfig"); len(k) > 0 {
+				*c = *client.GetConfigClient(k)
+			} else {
+				*c = *client.GetDefaultConfigClient()
+			}
+		},
 	}
 
-	// If config flag is set here...
-	c := client.GetDefaultConfigClient()
 	cmd.AddCommand(describeCmd(c))
 	cmd.AddCommand(getCmd(c))
 	cmd.AddCommand(logsCmd(c))
@@ -28,6 +39,7 @@ func cmd() *cobra.Command {
 	cmd.PersistentFlags().StringSliceP("context", "x", nil, "Specify the context(s) to operate in")
 	cmd.PersistentFlags().StringP("namespace", "n", "", "Specify the namespace within all the contexts specified")
 	cmd.PersistentFlags().StringArrayP("label", "l", nil, "Filter objects by label")
+	cmd.PersistentFlags().String("kubeconfig", "", "Custom kubeconfig file")
 
 	return cmd
 }
