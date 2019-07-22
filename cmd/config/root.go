fae29554d4951fe43f567bfd62a945bb11b505e1
Justin Pu <12262539+aurpine@users.noreply.github.com>
2019-07-22 16:40:33 -0700

    Add config subcommand

---
+++ cmd/config/root.go
@@ -0,0 +1,19 @@
+package config
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/wish/ctl/pkg/client"
+)
+
+// Cmd returns the config subcommand
+func Cmd(c *client.Client) *cobra.Command {
+	config := &cobra.Command{
+		Use:   "config",
+		Short: "Edit ctl configuration",
+		Long:  "Tool for changing the behaviour of ctl",
+	}
+
+	config.AddCommand(fetchCmd(c))
+
+	return config
+}
