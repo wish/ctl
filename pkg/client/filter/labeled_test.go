13f6f2d0ba902163aceea5ec7ef8e3139b55572b
Justin Pu <12262539+aurpine@users.noreply.github.com>
2019-07-31 17:34:48 -0700

    Add filter testing and change GetLabeled to make deep copy

---
+++ pkg/client/filter/labeled_test.go
@@ -0,0 +1,31 @@
+package filter
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestLabeled(t *testing.T) {
+	tests := []map[string]string{
+		nil,
+		map[string]string{},
+		map[string]string{"a": "b"},
+		map[string]string{"a": "b", "c": "d"},
+	}
+
+	for _, test := range tests {
+		l := GetLabeled(test)
+		if !reflect.DeepEqual(test, l.GetLabels()) {
+			t.Error("GetLabeled failed on", test)
+		}
+	}
+	// Custom test for deep copy.
+	a := map[string]string{"a": "b"}
+	l := GetLabeled(a)
+	b := a
+	a = map[string]string{"a": "c"}
+	if reflect.DeepEqual(a, l.GetLabels()) || !reflect.DeepEqual(b, l.GetLabels()) {
+		t.Error("GetLabeled failed on custom dynamic test")
+		// If this test is failing, then GetLabeled made a shallow copy of the map
+	}
+}
--- pkg/client/filter/labels_test.go
+++ pkg/client/filter/labels_test.go
@@ -9,32 +9,40 @@ func TestLabelMatchEq(t *testing.T) {
 		match  *LabelMatchEq
 		labels map[string]string
 		ans    bool
+		ans2   bool
 	}{
 		{
 			&LabelMatchEq{"a", "b"},
 			map[string]string{"a": "b"},
+			true,
 			true,
 		},
 		{
 			&LabelMatchEq{"a", "b"},
 			map[string]string{"a": "c"},
 			false,
+			false,
 		},
 		{
 			&LabelMatchEq{"a", "b"},
 			map[string]string{"b": "a"},
 			false,
+			true,
 		},
 		{
 			&LabelMatchEq{"a", "b"},
 			nil,
 			false,
+			true,
 		},
 	}
 
 	for _, i := range tests {
 		if i.match.Match(i.labels) != i.ans {
-			t.Error("Test failed", i.match, i.labels, i.ans)
+			t.Error("Test failed Match", i.match, i.labels, i.ans)
+		}
+		if i.match.EmptyOrMatch(i.labels) != i.ans2 {
+			t.Error("Test failed EmptyOrMatch", i.match, i.labels, i.ans2)
 		}
 	}
 }
@@ -44,32 +52,40 @@ func TestLabelMatchNeq(t *testing.T) {
 		match  *LabelMatchNeq
 		labels map[string]string
 		ans    bool
+		ans2   bool
 	}{
 		{
 			&LabelMatchNeq{"a", "b"},
 			map[string]string{"a": "b"},
+			false,
 			false,
 		},
 		{
 			&LabelMatchNeq{"a", "b"},
 			map[string]string{"a": "c"},
 			true,
+			true,
 		},
 		{
 			&LabelMatchNeq{"a", "b"},
 			map[string]string{"b": "a"},
+			true,
 			true,
 		},
 		{
 			&LabelMatchNeq{"a", "b"},
 			nil,
 			true,
+			true,
 		},
 	}
 
 	for _, i := range tests {
 		if i.match.Match(i.labels) != i.ans {
-			t.Error("Test failed", i.match, i.labels, i.ans)
+			t.Error("Test failed Match", i.match, i.labels, i.ans)
+		}
+		if i.match.EmptyOrMatch(i.labels) != i.ans2 {
+			t.Error("Test failed EmptyOrMatch", i.match, i.labels, i.ans2)
 		}
 	}
 }
@@ -79,37 +95,46 @@ func TestLabelMatchSetIn(t *testing.T) {
 		match  *LabelMatchSetIn
 		labels map[string]string
 		ans    bool
+		ans2   bool
 	}{
 		{
 			&LabelMatchSetIn{"a", []string{"b"}},
 			map[string]string{"a": "b"},
+			true,
 			true,
 		},
 		{
 			&LabelMatchSetIn{"a", []string{"b", "a"}},
 			map[string]string{"a": "c"},
 			false,
+			false,
 		},
 		{
 			&LabelMatchSetIn{"a", []string{"a", "b", "c"}},
 			map[string]string{"a": "b"},
+			true,
 			true,
 		},
 		{
 			&LabelMatchSetIn{"b", []string{"a", "b", "c"}},
 			map[string]string{"a": "b"},
 			false,
+			true,
 		},
 		{
 			&LabelMatchSetIn{"a", []string{"b"}},
 			nil,
 			false,
+			true,
 		},
 	}
 
 	for _, i := range tests {
 		if i.match.Match(i.labels) != i.ans {
-			t.Error("Test failed", i.match, i.labels, i.ans)
+			t.Error("Test failed Match", i.match, i.labels, i.ans)
+		}
+		if i.match.EmptyOrMatch(i.labels) != i.ans2 {
+			t.Error("Test failed EmptyOrMatch", i.match, i.labels, i.ans2)
 		}
 	}
 }
@@ -119,32 +144,40 @@ func TestLabelMatchMultiple(t *testing.T) {
 		match  *LabelMatchMultiple
 		labels map[string]string
 		ans    bool
+		ans2   bool
 	}{
 		{
 			&LabelMatchMultiple{[]LabelMatch{&LabelMatchEq{"a", "b"}, &LabelMatchNeq{"c", "k"}}},
 			map[string]string{"a": "b"},
+			true,
 			true,
 		},
 		{
 			&LabelMatchMultiple{[]LabelMatch{&LabelMatchEq{"a", "b"}, &LabelMatchSetIn{"b", []string{"b", "a"}}}},
 			map[string]string{"a": "b", "b": "b"},
 			true,
+			true,
 		},
 		{
 			&LabelMatchMultiple{[]LabelMatch{&LabelMatchEq{"a", "b"}, &LabelMatchSetIn{"a", []string{"b", "a"}}}},
 			map[string]string{"a": "a"},
+			false,
 			false,
 		},
 		{
 			&LabelMatchMultiple{nil},
 			map[string]string{"a": "b", "b": "b"},
 			true,
+			true,
 		},
 	}
 
 	for _, i := range tests {
 		if i.match.Match(i.labels) != i.ans {
-			t.Error("Test failed", i.match, i.labels, i.ans)
+			t.Error("Test failed Match", i.match, i.labels, i.ans)
+		}
+		if i.match.EmptyOrMatch(i.labels) != i.ans2 {
+			t.Error("Test failed EmptyOrMatch: ", i.match, i.labels, i.ans2)
 		}
 	}
 }
