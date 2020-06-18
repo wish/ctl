ccf9a3fadff6533a95a3f8946a0d571750dff92d
Priyanka Hariharan <phariharan@wish.com>
2020-06-18 13:39:01 -0400

    Allow prelogin command field from ctl.jsonnet to be empty

--- cmd/up.go
+++ cmd/up.go
@@ -23,8 +23,8 @@ type runDetails struct {
 	Resources    resource `json:"resources"`
 	Active       bool     `json:"active"`
 	Manifest     string   `json:"manifest"`
-	PreLogin	 []string `json:"pre_login"`
-	LoginCommand []string `json:"login_command,omitempty"`
+	PreLogin	 []string `json:"pre_login_command,omitempty"`
+	LoginCommand []string `json:"login_command"`
 }
 
 type resource struct {
